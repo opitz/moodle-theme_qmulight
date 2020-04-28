43b756467842e50a7977138a779db24b3b1510cd
Matthias Opitz <opitz@gmx.de>
2020-04-28 19:35:40 +0100

    added redir to landing pages

--- lib.php
+++ lib.php
@@ -4,4 +4,14 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-// We will add callbacks here as we add features to our theme.
+function theme_qmulight_page_init(moodle_page $page)
+{
+    global $CFG, $DB, $USER;
+
+    if ($page->pagelayout == 'mydashboard' && class_exists('\block_landingpage\landingpage')) {
+        $redir = \block_landingpage\landingpage::instance()->get_landing_page($USER);
+        if ($redir) {
+            redirect($redir);
+        }
+    }
+}
