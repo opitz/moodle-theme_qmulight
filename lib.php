0b0f99be2136504f7da2eeb0e1fbfb64a49eeeb0
Matthias Opitz <opitz@gmx.de>
2020-04-29 14:08:46 +0200

    Added own settings - no changes yet

--- lib.php
+++ lib.php
@@ -15,3 +15,29 @@ function theme_qmulight_page_init(moodle_page $page)
         }
     }
 }
+
+function theme_qmulight_get_main_scss_content($theme) {
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+
+    $context = context_system::instance();
+    if ($filename == 'default.scss') {
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_qmulight', 'preset', 0, '/', $filename))) {
+        // This preset file was fetched from the file area for theme_qmulight and not theme_boost (see the line above).
+        $scss .= $presetfile->get_content();
+    } else {
+        // Safety fallback - maybe new installs etc.
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+
+    return $scss;
+}
