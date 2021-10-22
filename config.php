de57632bcce47f92d31dc3e57ea3bb31b0c0a013
Matthias Opitz <opitz@gmx.de>
2021-10-22 17:18:46 +0100

    showing all elements of the navbar in light colours against blue background, added green background colour when editig switch is ON

--- config.php
+++ config.php
@@ -46,6 +46,13 @@ $THEME->requiredblocks = '';
 // it forces a block region into the page when editing is enabled and it takes up too much room.
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
 
+$THEME->extrascsscallback = 'theme_boost_get_extra_scss';
+$THEME->prescsscallback = 'theme_boost_get_pre_scss';
+$THEME->precompiledcsscallback = 'theme_boost_get_precompiled_css';
+$THEME->iconsystem = \core\output\icon_system::FONTAWESOME;
+$THEME->haseditswitch = true;
+
+
 // This is the function that returns the SCSS source for the main file in our theme. We override the boost version because
 // we want to allow presets uploaded to our own theme file area to be selected in the preset list.
 $THEME->scss = function($theme) {
